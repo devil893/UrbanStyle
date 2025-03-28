d95a92ec5053384ef18e2cb4e16b03933b594c92
Muhammad Hassan Khalid <hassankhalid768768@gmail.com>
2025-03-28 16:44:17 +0500

    Dual Home Page Problem Fixed

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import Navigation from './customer/components/Navigation/Navigation';
 import HomePage from './customer/pages/HomePage/HomePage';
 import Footer from './customer/components/Footer/Footer';
-import HomePage from './customer/pages/HomePage/HomePage'; 
+
 function App() {
   return (
     <div className="">
