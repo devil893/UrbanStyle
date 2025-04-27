dcb0d48687b06c2c1cb74c9ae1ea69499cd12583
Muhammad Hassan Khalid <hassankhalid768768@gmail.com>
2025-04-28 01:19:34 +0500

    some error occured so resetting github

--- node api/ecommerce-api-node/src/server.js
+++
@@ -1,9 +0,0 @@
-const { connect } = require("mongoose");
-const { connectDb } = require("./config/db");
-const app = require(".");
-const PORT = 5454;
-
-app.listen(PORT, async() => {
-    await connectDb
-    console.log("Ecommerce API listening on PORT:", PORT);
-});
